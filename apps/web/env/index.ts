488a1e6425420792ed96ee862c3fcdfd0b3866f7
pedrodevelops <dev.henriquegn@gmail.com>
2024-08-14 14:16:33 -0300

    fix: env schema is no longer used while in the client

--- apps/web/env/index.ts
+++ apps/web/env/index.ts
@@ -1,5 +1,7 @@
 import { z } from "zod";
 
+// TODO: Verify this server-side environment variables somewhere, since
+// these are not available in client-side code.
 const envSchema = z.object({
   PORT: z.string(),
   NODE_ENV: z.string(),
@@ -10,9 +12,7 @@ const envSchema = z.object({
 const parseAttempt = envSchema.safeParse(process.env);
 
 if (parseAttempt.success == false) {
-  throw new Error("Invalid environment variables", {
-    cause: parseAttempt.error,
-  });
+  throw new Error("Invalid environment variables: ");
 }
 
 export const env = parseAttempt.data;
