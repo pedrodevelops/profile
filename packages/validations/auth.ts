aaa1b56dd03d382a294b97385768a82b825db66c
pedrodevelops <dev.henriquegn@gmail.com>
2024-08-14 01:45:05 -0300

    feat: add authentication schemas to validations package

--- packages/validations/auth.ts
+++ packages/validations/auth.ts
@@ -1,6 +1,23 @@
+import z from "zod";
+
 export type HealthResponse = {
   status: "UP" | "DOWN";
   timestamp: string;
   uptime: number;
   environment: "development";
 };
+
+export const signUpSchema = z.object({
+  name: z.string().min(3).max(32),
+  email: z.string().email(),
+  password: z.string().min(8).max(32),
+});
+
+export type SignUpInput = z.infer<typeof signUpSchema>;
+
+export const signInSchema = z.object({
+  name: z.string().min(3).max(32),
+  password: z.string().min(8).max(32),
+});
+
+export type SignInInput = z.infer<typeof signInSchema>;
