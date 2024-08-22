12f4b6958b4a83a589235f1ac1e54cbdeb18ec3d
pedrodevelops <dev.henriquegn@gmail.com>
2024-08-21 22:27:32 -0300

    fix: adjust env vars usage in API

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -2,17 +2,19 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
 import { ZodExceptionFilter } from './common/filters/zod-exception.filter';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const configService = app.get(ConfigService);
+
   app.useGlobalFilters(new ZodExceptionFilter());
   app.use(cookieParser());
   app.enableCors({
-    // TODO: Change this to get the URL from an environment variable.
-    origin: 'http://localhost:3000',
+    origin: configService.getOrThrow('WEB_APP_URL'),
     credentials: true,
   });
-
-  await app.listen(3001);
+  await app.listen(parseInt(configService.getOrThrow('API_PORT')));
 }
 bootstrap();
